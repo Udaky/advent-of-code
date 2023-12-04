b7c8214cc5ee2049d486d6da59c0c8600dc92ac8
Udaky <rizkyfakhri227@gmail.com>
2023-12-04 22:20:36 +0700

    aoc

--- 2023/Js/day-2/index.js
+++ 2023/Js/day-2/index.js
@@ -27,25 +27,30 @@ const maxCount = {
 //     }, 0);
 // };
 
-const partOne = (file) => {
+const partTwo = (file) => {
   const lines = fs.readFileSync(file, "utf-8").split("\r\n");
   return lines
     .map((line) => {
-      return line
+      const maxCount = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
+      line
         .split(": ")[1]
         .split("; ")
-        .map((set) => {
+        .forEach((set) => {
           const pulls = set.split(", ");
-          return pulls.every((pull) => {
+          return pulls.forEach((pull) => {
             const [count, color] = pull.split(" ");
-            return maxCount[color] >= Number(count);
+            if (maxCount[color] < Number(count)) {
+              maxCount[color] = Number(count);
+            }
           });
-        })
-        .every((p) => p);
+        });
+      return maxCount.red * maxCount.green * maxCount.blue;
     })
-    .reduce((s, result, i) => {
-      return result ? s + (i + 1) : s;
-    }, 0);
+    .reduce((s, v) => s + v);
 };
 
-console.log(partOne("./example.txt"));
+console.log(partTwo("./data.txt"));
